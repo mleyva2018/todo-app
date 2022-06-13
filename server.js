e04edf2aa746e738bb0852e288e6b8e66e08ffcf
Manuel <45129627+mleyva2018@users.noreply.github.com>
2022-06-12 23:32:51 -0600

    first commit

---
+++ server.js
@@ -0,0 +1,120 @@
+let express = require("express")
+let { MongoClient, ObjectId } = require("mongodb")
+let sanitizeHTML = require("sanitize-html")
+
+let app = express()
+let db
+
+let port = process.env.PORT
+if (port == null || port == "") {
+  port = 3000
+}
+
+app.use(express.static("public"))
+
+let main = async () => {
+  let client = new MongoClient(
+    "mongodb+srv://Judomaster:Saludos2026!@cluster0.xxg06.mongodb.net/TodoApp?retryWrites=true&w=majority"
+  )
+  await client.connect()
+  db = client.db()
+  app.listen(port)
+}
+
+main()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+function passwordProtected(req, res, next) {
+  res.set("WWW-Authenticate", 'Basic realm="Simple Todo App"')
+  console.log(req.headers.authorization)
+  if (req.headers.authorization == "Basic bWxleXZhOlNhbHVkb3MyMDI3IQ==") {
+    next()
+  } else {
+    res.status(401).send("Authentication required")
+  }
+}
+
+app.use(passwordProtected)
+
+app.get("/", (req, res) => {
+  db.collection("items")
+    .find()
+    .toArray((err, items) => {
+      res.send(`<!DOCTYPE html>
+      <html>
+        <head>
+          <meta charset="UTF-8" />
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+          <title>Simple To-Do App</title>
+          <link
+            rel="stylesheet"
+            href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css"
+            integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS"
+            crossorigin="anonymous"
+          />
+        </head>
+        <body>
+          <div class="container">
+            <h1 class="display-4 text-center py-1">Manuel's things to do:</h1>
+      
+            <div class="jumbotron p-3 shadow-sm">
+              <form id="create-form" action="/create-item" method="POST">
+                <div class="d-flex align-items-center">
+                  <input id="create-field"
+                    name="item"
+                    autofocus
+                    autocomplete="off"
+                    class="form-control mr-3"
+                    type="text"
+                    style="flex: 1"
+                  />
+                  <button class="btn btn-primary">Add New Item</button>
+                </div>
+              </form>
+            </div>
+      
+            <ul id="item-list" class="list-group pb-5">
+
+            </ul>
+          </div>
+          <script>
+          let items = ${JSON.stringify(items)}
+          </script>
+          <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+          <script src="/browser.js"></script>
+        </body>
+      </html>`)
+    })
+})
+
+app.post("/create-item", (req, res) => {
+  let safeText = sanitizeHTML(req.body.text, {
+    allowedTags: [],
+    allowedAttributes: {},
+  })
+  db.collection("items").insertOne({ text: safeText }, (err, info) => {
+    res.json({ _id: info.insertedId, text: safeText })
+  })
+})
+
+app.post("/update-item", (req, res) => {
+  let safeText = sanitizeHTML(req.body.text, {
+    allowedTags: [],
+    allowedAttributes: {},
+  })
+  db.collection("items").findOneAndUpdate(
+    { _id: new ObjectId(req.body.id) },
+    { $set: { text: safeText } },
+    () => {
+      res.send("success!!!")
+    }
+  )
+})
+
+app.post("/delete-item", (req, res) => {
+  db.collection("items").deleteOne({ _id: new ObjectId(req.body.id) }, () => {
+    res.send("Success!")
+  })
+})
